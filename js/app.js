08fb03329590311fd1a6d7bc07fe99c13feba0d3
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-15 14:43:43 -0400

    solve exercise 2

--- js/app.js
+++ js/app.js
@@ -12,7 +12,10 @@ console.log('Exercise 1:', multiply(4, 5));
 // Create a function that takes an integer as an argument and returs "Even" for even numbers or "Odd" for odd numbers.
 
 function evenOrOdd(num) {
-  
+  // Use ternary operator to conditionally display "Even" or "Odd"
+  // If the number is evenly divisible by 2 (no remainder), return "Even"
+  // If there is no remainder (0 is falsy) use Bang operator (!) to convert value into truthy
+  return !(num % 2) ? 'Even' : 'Odd'
 }
 
 console.log(evenOrOdd(8));
