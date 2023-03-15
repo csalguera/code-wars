aed8808b4efcb7c91b8bc44e3902fe27439c00fd
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-15 14:37:30 -0400

    stub up exercise 2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 //! Multiply
 // This code does not execute properly. Try to figure out why.
 
-// Problem
-
 function multiply(a, b) {
   return a * b
   // Code did not return anything, hence undefined
 }
 
-console.log(multiply(4, 5));
+console.log('Exercise 1:', multiply(4, 5));
+
+//! Even or Odd
+// Create a function that takes an integer as an argument and returs "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(num) {
+  
+}
+
+console.log(evenOrOdd(8));
