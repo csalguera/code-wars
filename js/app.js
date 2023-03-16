301d51f6cc297ec73b0c0a6ecf662d94de7c3ae3
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-16 11:22:37 -0400

    stub up exercise 7

--- js/app.js
+++ js/app.js
@@ -107,3 +107,14 @@ function digitize(n) {
 }
 
 console.log(`Exercise 7: ${digitize(1234)}`)
+
+//! Ones and Zeros
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+const binaryArrayToNumber = arr => {
+  return arr
+}
+
+console.log(`Exercise 8: ${binaryArrayToNumber([0, 1, 1, 0])} is equal to 6`);
